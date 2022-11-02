950a0e52d9629a68c3381210de1a512117b0964b
Koumarin <alicefigueiredo837@gmail.com>
2022-11-02 15:06:20 -0300

    add checks for flying and swimming mounts
    If the player can fly, now it only looks for flying mounts, same thing if the player is in water.
    
    Doesn't check for specific mount skill requirements or skills other than expert riding at the moment.

--- random-mount.lua
+++ random-mount.lua
@@ -1,3 +1,48 @@
+local F = CreateFrame("Frame")
+
 function CallRandomMount(mountType)
-	CallCompanion("MOUNT", random(GetNumCompanions("MOUNT")))
+	local M  = {}                        -- List of our mounts.
+	local sz = GetNumCompanions("MOUNT") -- Number of mounts we have.
+
+	for i = 1, sz do M[i] = i end      -- Fill list with mounts.
+
+	-- Creates a predicate that returns true for mounts of matching type.
+	local p = function(type)
+		return function(elt)
+			local eltType = select(6, GetCompanionInfo("MOUNT", elt))
+			return 0 ~= bit.band(eltType, type)
+		end
+	end
+
+	if mountType then                  -- If player is asking for a specific
+		M = F:Filter(M, p(mountType))  -- mount type, look for that type.
+	elseif F:CanFly() then             -- Otherwise, if we can fly,
+		M = F:Filter(M, p(0x2))        -- we take only flying mounts.
+	elseif IsSwimming() then           -- If we are swimming,
+		M = F:Filter(M, p(0x8))        -- we take only water mounts.
+	else
+		M = F:Filter(M, function(elt)
+			local eltType = select(6, GetCompanionInfo("MOUNT", elt))
+			return 0 == bit.band(0x2, eltType) -- Take NOT flying mounts.
+		end)
+	end
+
+	CallCompanion("MOUNT", M[random(#M)])
+end
+
+function F:CanFly()
+	-- 34090 is Expert Riding.
+	return FindSpellBookSlotBySpellID(34090) and IsFlyableArea()
+end
+
+function F:Filter(A, p)
+	local B  = {} -- New array.
+
+	for _, x in ipairs(A) do
+		if p(x) then                   -- If x satisfies predicate p,
+			table.insert(B, x)         -- we insert x into B.
+		end
+	end
+
+	return B
 end
